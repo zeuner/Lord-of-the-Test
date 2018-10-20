a73fd78be0f435a31b84b8a1943a8c68f231593a
Isidor Zeuner <minetest@quidecco.de>
2018-10-20 11:23:35 +0200

    replace deprecated function call

--- mods/lottmapgen/init.lua
+++ mods/lottmapgen/init.lua
@@ -182,7 +182,7 @@ minetest.register_on_generated(function(minp, maxp, seed)
 	nobj_random = nobj_random or minetest.get_perlin_map(np_random, chulens)
 
 	local nvals_temp = nobj_temp:get_2d_map_flat(minposxz, nbuf_temp)
-	local nvals_humid = nobj_humid:get2dMap_flat(minposxz, nbuf_humid)
+	local nvals_humid = nobj_humid:get_2d_map_flat(minposxz, nbuf_humid)
 	local nvals_random = nobj_random:get_2d_map_flat(minposxz, nbuf_random)
 
 	local offset = math.random(5,20)
